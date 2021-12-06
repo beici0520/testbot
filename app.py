da97935a30697ae4ff1301c7876f9fee005e8e4f
beici0520 <95210070+beici0520@users.noreply.github.com>
2021-12-06 17:12:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,9 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage("這是學校網址https://www.ntut.edu.tw/")
+    message123 = event.message.text
+    if re.match("學校網址"，message123):
+    message456 = TextSendMessage("這是學校網址https://www.ntut.edu.tw/")
     line_bot_api.reply_message(event.reply_token,message)
 
 #主程式
