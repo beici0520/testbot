0506ed2883a3275dcfc452777b48b95114434af2
beici0520 <95210070+beici0520@users.noreply.github.com>
2021-12-13 16:44:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,13 +50,13 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	message = text =event.message.text
-	if re.match('問卷',message):
-		#message = TextSendMessage(text=event.message.text)
-		text_message = TextSendMessage(text="官方網站 : https://cruelshare.com/")
-        	line_bot_api.reply_message(event.reply_token, text_message)
-	else:
-		line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+	#message = text =event.message.text
+	#if re.match('問卷',message):
+	message = TextSendMessage(text=event.message.text)
+		#text_message = TextSendMessage(text="官方網站 : https://cruelshare.com/")
+        	#line_bot_api.reply_message(event.reply_token, text_message)
+	#else:
+		#line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 	
 #主程式
 import os
