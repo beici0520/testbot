bfd3df94cd076f4595ae9a3a5d0a4902bfb25d51
beici0520 <95210070+beici0520@users.noreply.github.com>
2021-12-13 16:33:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	message = text =event.message.text
-	if re.match('問卷',message):
+	if re.match("網址",message):
 		#message = TextSendMessage(text=event.message.text)
-		text_message = TextSendMessage(text="官方網站 : https://cruelshare.com/")
-        line_bot_api.reply_message(event.reply_token, text_message)
+		text_message = TextSendMessage(text="學校網址 : https://www.ntut.edu.tw/")
+       	 	line_bot_api.reply_message(event.reply_token, text_message)
 	
 
 
