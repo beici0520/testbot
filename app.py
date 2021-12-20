63f96f384941b70286ba75084dd9faeeb73a5c1a
beici0520 <95210070+beici0520@users.noreply.github.com>
2021-12-20 16:16:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  2 21:16:35 2021
-@author: Ivan
-版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
-Line Bot聊天機器人
-第一章 Line Bot申請與串接
-Line Bot機器人串接與測試
-"""
+
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
 
@@ -18,15 +11,15 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 import re
-
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('i7n2ZSGRJ3FWYTBq5UG1WVi3rVfc3hmNiu5W20ybPda8axBSFbUVWJvR2ZHodyVitw0gjYkCxz+tcc8Y+ywXj0sngKrFaUPeXfm7jm+WWXgSHr/dAMudkY+jNRNQ6XKoogBb3KHC8WdhAA2999W29QdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('自己的Channel Access Token')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('c9984b77fffa4f6e1e00690fe22a326e')
+handler = WebhookHandler('自己的Channel Secret')
 
-line_bot_api.push_message('Ue6d69def2d594a560ad9a768527c0bdd', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('你要推撥的user id', TextSendMessage(text='你可以開始了'))
+
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -34,6 +27,7 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+ 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
@@ -46,15 +40,52 @@ def callback():
 
     return 'OK'
 
+ 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message123 = event.message.text
-    if re.match("網址",message123):
-        text_message456 = TextSendMessage("這是學校網址https://www.ntut.edu.tw/")
-        line_bot_api.reply_message(event.reply_token, text_message456 )
-	
+    message = text=event.message.text
+
+    emoji = [
+    {
+        "index": 0,
+        "productId": "5ac1bfd5040ab15980c9b435",
+        "emojiId": "001"
+    },
+    {
+        "index": 2,
+        "productId": "5ac1bfd5040ab15980c9b435",
+        "emojiId": "002"
+    },
+    {
+        "index": 15,
+        "productId": "5ac1bfd5040ab15980c9b435",
+        "emojiId": "002"
+    }
+    ]
+
+    
+    
+    if re.match('拍照',message):
+        flex_message = TextSendMessage(text='以下有雷，請小心',
+        quick_reply=QuickReply(items=[
+        QuickReplyButton(action=CameraAction(label="拍照")),
+        QuickReplyButton(action=MessageAction(label="按我", text="按！")),
+        QuickReplyButton(action=MessageAction(label="別按我", text="你按屁喔！爆炸了拉！！"))]))
+        line_bot_api.reply_message(event.reply_token, flex_message)
+    if re.match('連結',message):
+        text_message = TextSendMessage(text="連結 : https://cruelshare.com/")
+        line_bot_api.reply_message(event.reply_token, text_message)
+    if re.match('emoji',message):
+        text_message = TextSendMessage(text='$ $ LINE emoji $', emojis=emoji)
+        line_bot_api.reply_message(event.reply_token, text_message)
+    
+    """
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    """
+
 #主程式
 import os
 if __name__ == "__main__":
